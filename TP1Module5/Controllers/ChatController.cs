039f8c6bf8e312947d6baffa9ac04dcf05512f72
Delphine OMILLI <delphine.omilli2020@campus-eni.fr>
2020-09-16 16:50:08 +0200

    TP CHAT

---
+++ TP1Module5/Controllers/ChatController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP1Module5.Database;
+
+namespace TP1Module5.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View(FakeDb.Instance.GetMeuteDeChats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(FakeDb.Instance.GetMeuteDeChats.FirstOrDefault(chat => chat.Id == id));
+        }
+
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TP1Module5/Database/FakeDb.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TP1Module5.Models;
+
+namespace TP1Module5.Database
+{
+    public class FakeDb
+    {
+
+        private static readonly Lazy<FakeDb> lazy =
+            new Lazy<FakeDb>(() => new FakeDb());
+
+        /// <summary>
+        /// FakeDb singleton access.
+        /// </summary>
+        public static FakeDb Instance { get { return lazy.Value; } }
+
+        private FakeDb()
+        {
+            this.GetMeuteDeChats = new List<Chat>();
+            this.InitialiserDatas();
+        }
+
+        public List<Chat> GetMeuteDeChats { get; private set; }
+
+
+        private void InitialiserDatas()
+        {
+            var i = 1;
+
+            GetMeuteDeChats.Add(new Chat { Id = i++, Nom = "Felix", Age = 3, Couleur = "Roux" });
+            GetMeuteDeChats.Add(new Chat { Id = i++, Nom = "Minette", Age = 1, Couleur = "Noire" });
+            GetMeuteDeChats.Add(new Chat { Id = i++, Nom = "Miss", Age = 10, Couleur = "Blanche" });
+            GetMeuteDeChats.Add(new Chat { Id = i++, Nom = "Garfield", Age = 6, Couleur = "Gris" });
+            GetMeuteDeChats.Add(new Chat { Id = i++, Nom = "Chatran", Age = 4, Couleur = "Fauve" });
+            GetMeuteDeChats.Add(new Chat { Id = i++, Nom = "Minou", Age = 2, Couleur = "Blanc" });
+            GetMeuteDeChats.Add(new Chat { Id = i, Nom = "Bichette", Age = 12, Couleur = "Rousse" });
+
+        }
+    }
+}
